891483a1b4dcfb7f3dc07502698d0f1f76ecacfd
desertdweller15 <bcoutee11@gmail.com>
2016-07-21 19:41:15 -0700

    Added data author atribute and script tag to index doc

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -32,7 +32,9 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('article').hide();
+      var $value=$(this).val()
+      $('article[data-author="' +$value+ '"]').show();
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
@@ -73,4 +75,7 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
-$();
+$(function(){
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
+});
